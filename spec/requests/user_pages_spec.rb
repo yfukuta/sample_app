980ba834e08582e0aaae1141e9bf27bfa208557e
Youji Fukuta <y-fukuta@m2.dion.ne.jp>
2013-11-29 22:22:00 +0900

    Make a basic User model (including secure passwords)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,14 +2,35 @@ require 'c:/sites/sample_app/spec/spec_helper.rb'
 
 require 'spec_helper'
 
-describe "User pages" do
+describe User do
 
-  subject { page }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
-  describe "signup page" do
-    before { visit signup_path }
+  subject { @user }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+  .
+  .
+  .
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
   end
 end
