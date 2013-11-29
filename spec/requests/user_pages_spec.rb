e54aa4040f5af160e7050806610ef2c10e153353
Youji Fukuta <y-fukuta@m2.dion.ne.jp>
2013-11-29 14:18:50 +0900

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'c:/sites/sample_app/spec/spec_helper.rb'
+
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+end
