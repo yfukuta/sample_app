c6d5d890b5ad0c61fda9843a473e64eb4753e565
Youji Fukuta <y-fukuta@m2.dion.ne.jp>
2013-12-01 20:42:46 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   #get "static_pages/home"
   #get "static_pages/help"
   match '/signup', to: 'users#new',  via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/help', to: 'static_pages#help', via: 'get'
   #get "static_pages/about"
   match '/about',   to: 'static_pages#about',   via: 'get'
